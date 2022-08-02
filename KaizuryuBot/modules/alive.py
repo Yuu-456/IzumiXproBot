38508b41557e471b4683221f9d3c0f765378a021
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-02 08:13:20 +0530

    Update alive.py

--- KaizuryuBot/modules/alive.py
+++ KaizuryuBot/modules/alive.py
@@ -11,20 +11,20 @@ from KaizuryuBot import telethn as tbot, SUPPORT_CHAT, OWNER_USERNAME, dispatche
 from KaizuryuBot import ALIVE_PIC_URL
 
 
-PHOTO = ALIVE_PIC_URL
+PHOTO = "https://telegra.ph/file/a245a547b0939797c4489.jpg"
 
 
 @register(pattern=("/alive"))
 async def awake(event):
     TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : @{OWNER_USERNAME} ** \n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : @IzumiXTachibana ** \n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
     BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", f"https://t.me/{dispatcher.bot.username}?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", f"https://t.me/{SUPPORT_CHAT}"),
+            Button.url("ʜᴇʟᴘ​", f"https://t.me/MiyamuraXproBot?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", f"https://t.me/MiyamuraXsupport"),
         ]
     ]
     ran = PHOTO
